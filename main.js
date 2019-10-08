6cefa4712959780582e141cebca7500144ac2b3d
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-08 14:11:13 -0400

    assignment nearly complete

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-1
+const paragraph = document.querySelector('p');
+paragraph.style.color = 'lightBlue'
+
+// document.querySelector('p').style.color = 'lightBlue'
+
+const heading = document.querySelector('h1');
+heading.style.fontSize = '10px';
+
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+const item13 = document.querySelector('#item-13');
+
+item13.style.opacity = '50%';
+
+const item3 = document.querySelector('#item-3');
+
+item3.innerText = 'I say, "Hi!';
+
+const image = document.querySelector('img');
+
+image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg"
+
+image.style.height = '300px';
+
+const image2 = document.createElement('img');
+image2.src = "https://cdn1.cyclist.co.uk/sites/cyclist/files/styles/article_main_wide_image/public/6/44/climbing_steep_hills_0.jpg?itok=yLwzoV6F";
+
+const div = document.querySelector('div');
+div.appendChild(image2);
+image2.style.height = '300px';
+
+// const newLi = document.createElement('.item');
+
+
+
+
